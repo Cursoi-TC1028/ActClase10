f5b335d79345a4ff082f96f00e69b027912dd748
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-22 18:27:46 -0700

    Update exercise.py

--- assignments/03Ejercicio/src/exercise.py
+++ assignments/03Ejercicio/src/exercise.py
@@ -1,23 +1,6 @@
-
-def funcion(va1, va2, clave) :
-    if clave=="s":
-        return va1+va2
-    elif clave=="r":
-        return va1-va2
-    elif clave=="d":
-          return va1/va2
-    elif clave=="m":
-          return va1*va2
-
-    
-
 def main() :
-    # pido el tipo de silla (B E L)
-    v1 = int(input("introducir valor 1: "))
-    v2 = int(input("introducir valor 2: "))
-    # pido el tipo de cliente (N F)
-    c = input("introducir clave  (s, r, m, d): ")
-    print("resultado:",funcion(v1,v2,c))
+   #introduce el codigo
+   pass
 
 
 
